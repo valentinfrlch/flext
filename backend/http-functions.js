b0c550db340a90674d7220d4e5640dbec30fc739
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-06-05 20:21:42 +0200

    Update http-functions.js

--- backend/http-functions.js
+++ backend/http-functions.js
@@ -4,13 +4,6 @@ import { pull } from 'backend/cloud';
 import { DynamicUploadSystem } from 'backend/updateTrends';
 import wixData from 'wix-data';
 
-// URL to call this HTTP function from your published site looks like: 
-// Premium site - https://mysite.com/_functions/example/multiply?leftOperand=3&rightOperand=4
-// Free site - https://username.wixsite.com/mysite/_functions/example/multiply?leftOperand=3&rightOperand=4
-
-// URL to test this HTTP function from your saved site looks like:
-// Premium site - https://mysite.com/_functions-dev/example/multiply?leftOperand=3&rightOperand=4
-// Free site - https://username.wixsite.com/mysite/_functions-dev/example/multiply?leftOperand=3&rightOperand=4
 
 export async function get_upload(request) {
     const response = {
