77be9adc1d0d198c8c1ca9d8ee342a125e7af9e6
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-06-05 23:16:20 +0200

    Update fusion.js

--- backend/fusion.js
+++ backend/fusion.js
@@ -6,20 +6,20 @@ export async function fusion(titles) {
     let movieId;
     let tvId;
     const pro = titles.map(async element => {
-        var x = await fetch("https://api.themoviedb.org/3/search/movie?api_key=b47d0bbacc0cfd37962b6bb28118f099&language=de-DE&query=" + encodeURIComponent(element))
+        var x = await fetch("https://api.themoviedb.org/3/search/movie?api_key=APIKEY&language=de-DE&query=" + encodeURIComponent(element))
         var y = await x.json()
 
-        var w = await fetch("https://api.themoviedb.org/3/search/tv?api_key=b47d0bbacc0cfd37962b6bb28118f099&language=de-DE&query=" + encodeURIComponent(element))
+        var w = await fetch("https://api.themoviedb.org/3/search/tv?api_key=APIKEY&language=de-DE&query=" + encodeURIComponent(element))
         var q = await w.json()
 
         try {
             movieId = y.results[0].id
             tvId = q.results[0].id
         } catch (err) {console.log(err)}
-        var f = await fetch("https://api.themoviedb.org/3/movie/" + movieId + "/recommendations?api_key=b47d0bbacc0cfd37962b6bb28118f099&language=de-DE&page=1")
+        var f = await fetch("https://api.themoviedb.org/3/movie/" + movieId + "/recommendations?api_key=APIKEY&language=de-DE&page=1")
         var movieResults = await f.json()
 
-        var e = await fetch("https://api.themoviedb.org/3/tv/" + tvId + "/recommendations?api_key=b47d0bbacc0cfd37962b6bb28118f099&language=de-DE&page=1")
+        var e = await fetch("https://api.themoviedb.org/3/tv/" + tvId + "/recommendations?api_key=APIKEY&language=de-DE&page=1")
         var tvResults = await e.json()
 
         try {
