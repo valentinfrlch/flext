596c041e64d5f22fb4c146f8f5befa128c433e50
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-11-02 11:02:26 +0100

    Update maintain.py

--- server/node01/maintain.py
+++ server/node01/maintain.py
@@ -1,5 +1,10 @@
 import os
 from progress.bar import Bar
+import requests
+import json
+
+#secrets
+TMDB_KEY = "b47d0bbacc0cfd37962b6bb28118f099"
 
 moviePath = "/media/pi/F01/Flext/movies/"
 tvPath = "/media/pi/F01/Flext/tv/"
@@ -32,6 +37,40 @@ def tv():
                         print("    " + file + " -> " + file + ".mp4")
                         bar.next()
     bar.finish()
+
+def maintain(mode):
+    path = ""
+    if mode == "movie":
+        path = moviePath
+    else:
+        path = tvPath
+    for files in os.walk(path):
+        movieQuery = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" + TMDB_KEY + "&language=de-DE&query=" + file[0].replace(".mp4", ""))
+        tvQuery= requests.get("https://api.themoviedb.org/3/search/tv?api_key=" + TMDB_KEY + "&language=de-DE&query=" + file[0].replace(".mp4", ""))
+
+        movieQuery = movieQuery.json()
+        tvQuery = tvQuery.json()
+        match = ""
+        try:
+            if mode == "movie":
+                match = movieQuery["results"][0]["title"]
+            else:
+                match = tvQuery["results"][0]["title"]
+        except:
+            print("[404] no match found")
+        
+        #check if NOT 100% match:
+        if match != title:
+            print("No match found: ")
+            print(title, match)
+            ans = input("rename " + title + " -> " + match + "?")
+            if ans == "y":
+                os.rename("<oldPath>, <newPath>")
+            else:
+                print("not changing")
+        else:
+            print(title + " is a match.")
+
 
 movies()
 tv()
