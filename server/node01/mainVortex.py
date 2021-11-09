1e9847e4e9ffecb39f7f9080d114733423ff21ed
Valentin Fröhlich <85313672+valentinfrlch@users.noreply.github.com>
2021-11-09 07:53:57 +0100

    Update mainVortex.py

--- server/node01/mainVortex.py
+++ server/node01/mainVortex.py
@@ -1,14 +1,24 @@
 #/usr/bin/python3
 from phue import Bridge
 import time
+import threading
+from gpiozero import CPUTemperature
+import psutil
+NETWORK_INTERFACE = 'en0'
+netio = psutil.net_io_counters(pernic=True)
+
 
 v = Bridge('10.0.1.145')
 v.connect()
 
 v.get_api()
 
-#needs some threading: one thread to check server status
-# and one to control the lights. -> effects()
+# main funtions
+
+def monitor():
+    usageMatrix = []
+    usageMatrix.append(CPUTemperature()) #CPU
+    usageMatrix.append(netio[NETWORK_INTERFACE].bytes_sent + netio[NETWORK_INTERFACE].bytes_recv)
 
 
 
@@ -29,12 +39,24 @@ def effects(effect):
         effectTime = 1 #in seconds
         commands.append([effectTime, {'transitiontime' : 30, 'on' : True, 'bri' : 254}])
     if effect == "warning":
-        command =  {'transitiontime' : 30, 'on' : True, 'bri' : 254}
+        effectTime = 1 #in seconds
+        commands.append([effectTime, {'transitiontime' : 30, 'on' : True, 'bri' : 254}])
     if effect == "traffic":
-        command =  {'transitiontime' : 30, 'on' : True, 'bri' : 254}
+        effectTime = 1 #in seconds
+        commands.append([effectTime, {'transitiontime' : 30, 'on' : True, 'bri' : 254}])
     if effect == "welcome":
-        command =  {'transitiontime' : 30, 'on' : True, 'bri' : 254}
+        effectTime = 1 #in seconds
+        commands.append([effectTime, {'transitiontime' : 30, 'on' : True, 'bri' : 254}])
 
     for request in commands:
         v.set_light(7, request[1])
         time.sleep(request[0])
+
+
+
+
+
+
+
+lighting_thread = threading.Thread(target=thread_function, args=(1,))
+lighting_thread.start()
