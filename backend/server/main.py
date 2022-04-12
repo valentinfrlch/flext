77e53120d886889ad982f2251efffe396e977320
valentinfrlch <valentin.froehlich@gmail.com>
2022-04-12 08:33:57 +0100

    webdriver

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,8 +1,8 @@
 # main.py on backend upload server node01
 
 # import beautifulsoup4
-from turtle import ScrolledCanvas
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 import time
 
@@ -25,12 +25,20 @@ def search_movie(movie_name):
     link_frag = soup.find_all(
         "a", class_="film-poster-ahref flw-item-tip")[0].get("href")
     link = baseurl + link_frag.replace("/movie/", "/watch-movie/")
-    print(link)
-    # get movie stream url
-    soup = BeautifulSoup(requests.get(
-        link, headers=headers).text, "html.parser")
-    # load html 
-    print(soup.prettify())
+
+    # headless browser
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    dr = webdriver.Chrome(options=options)
+    s = requests.Session()
+    r = s.get(link, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+    # find all iframes with id "iframe-embed"
+    iframes = soup.find_all("iframe", id="iframe-embed")
+    # get the first iframe src
+    iframe_src = iframes[0].get("src")
+    print(iframe_src)
+
 
 
 search_movie("Uncharted")
