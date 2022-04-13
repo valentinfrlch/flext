a039974d0164d1852a6c1bd07a00589bd0b5455f
valentinfrlch <valentin.froehlich@gmail.com>
2022-04-13 15:01:15 +0100

    changes

--- backend/server/main.py
+++ backend/server/main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
 import time
+import urllib
 
 # search for movie on website
 
@@ -15,7 +16,7 @@ def search_movie(movie_name):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}
     baseurl = "https://hdtoday.cc"
     # encode movie for url
-    movie_name = movie_name.replace(" ", "+")
+    movie_name = urllib.parse.quote(movie_name)
     print(movie_name)
     url = baseurl + "/search/" + movie_name
     # get the first element on page
@@ -29,24 +30,34 @@ def search_movie(movie_name):
     # headless browser
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
-    dr = webdriver.Chrome(options=options)
+    dr = webdriver.Chrome()
     dr.get(link)
-    time.sleep(5)
+    # wait for page to load
+    time.sleep(10)
+    # click close button on popup
+    # find the button
+    close_button = dr.find_element_by_class_name("close")
+    
+    # click the button
+    close_button.click()
     soup = BeautifulSoup(dr.page_source, "html.parser")
     # select iframe and get src
     src = soup.find_all("iframe")[0].get("src")
+    dr.quit()
     return src, movie_name
 
 
 def download_handler(url, name):
-    # add the link to a file in the folder "folderwatch"
-    # open a new file in folderwatch
-    with open("/home/pi/folderwatch/" + name + ".txt", "w") as f:
-        f.write("text=" + url + "\n" + "filename=" +
-                name.replace("+", " ") + ".mp4")
+    #add the link to a file in the folder "folderwatch"
+    #open a new file in folderwatch
+    with open("/home/pi/folderwatch/" + name + ".crawljob", "w") as f:
+        f.write("\ntext=" + url + "\n" + "filename=" +
+                name.replace("&20", " ") + ".mp4" + "\n" + "packageName=" + movie_name.replace("&20", " ") +
+                "\ndeepAnalyseEnabled=true")
         # close file
         f.close()
 
 
-src, movie_name = search_movie("Uncharted")
+src, movie_name = search_movie("Bigbug")
+print(src, movie_name)
 download_handler(src, movie_name)
